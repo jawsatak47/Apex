af938b581f08bb547a501f122221a370299cd519
jawsatak47 <jawsatak47@gmail.com>
2026-03-30 15:07:11 -0700

    Create predators.js

---
+++ predators.js
@@ -0,0 +1,4 @@
+const PREDATORS_DATA = [
+  { name: "Great White", identity: "Ocean Hunter", image: "shark.jpg", stats: { power: 90 } },
+  { name: "Lion", identity: "Savanna King", image: "lion.jpg", stats: { power: 85 } }
+];
